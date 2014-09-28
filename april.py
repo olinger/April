91bb15e0314438da7a6ad266147469b2a6727ae4
Jazmine Olinger <olingj@rpi.edu>
2014-09-27 22:53:56 -0400

    does more of the things

--- april.py
+++ april.py
@@ -4,17 +4,16 @@ APPLICATION_ID = "15DQ4FEwTwxxXOTwjDKwDsyyZGLfTTZS9WTQlWaN"
 REST_API_KEY = "RIVWH59VFaPUqER09SjeJbxYA8NwDW1yOisuvBuv"
 
 from parse_rest.connection import register, ParseBatcher
-# Alias the Object type to make clear is not a normal python Object
 from parse_rest.datatypes import Object as ParseObject
 register(APPLICATION_ID, REST_API_KEY)
 
 anyObject = ParseObject()
 
 name = raw_input("Enter your name: ")
-city = raw_input("Where do you live? ")
+zipcode = raw_input("Enter your zip code: ")
 userObject = ParseObject()
 userObject.name = name
-userObject.city = city
+userObject.zipcode = int(zipcode)
 hates = []
 while True:
   hate = raw_input("Tell me something you hate: ")
@@ -22,7 +21,7 @@ while True:
     break
   else:
       hates.append(hate)
-  #also_hates(name, city, hate)
+
 userObject.hates = hates
 def saveToParse(anyObject):
     print "Saving..."
@@ -32,3 +31,9 @@ def saveToParse(anyObject):
     print "Done!"
     
 saveToParse(userObject)
+
+local = list(ParseObject.Query.filter(zipcode=userObject.zipcode))
+for person in local:
+  for item in userObject.hates:
+    if item in person.hates and person.name != userObject.name:
+      print person.name, "also hates" , item , "!"
