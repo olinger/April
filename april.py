68b582702bb53f854a97309e30db4a4b740e3c8f
Jazmine Olinger <olingj@rpi.edu>
2014-09-27 20:44:30 -0400

    does the things

--- april.py
+++ april.py
@@ -1,20 +1,34 @@
 import os, sys
 
-APPLICATION_ID = "APPLICATION_ID_HERE"
-REST_API_KEY = "REST_API_KEY_HERE"
+APPLICATION_ID = "15DQ4FEwTwxxXOTwjDKwDsyyZGLfTTZS9WTQlWaN"
+REST_API_KEY = "RIVWH59VFaPUqER09SjeJbxYA8NwDW1yOisuvBuv"
 
 from parse_rest.connection import register, ParseBatcher
 # Alias the Object type to make clear is not a normal python Object
 from parse_rest.datatypes import Object as ParseObject
-
 register(APPLICATION_ID, REST_API_KEY)
-anyObject.title = 'Hello world'
-anyObject.score = 100
 
+anyObject = ParseObject()
 
+name = raw_input("Enter your name: ")
+city = raw_input("Where do you live? ")
+userObject = ParseObject()
+userObject.name = name
+userObject.city = city
+hates = []
+while True:
+  hate = raw_input("Tell me something you hate: ")
+  if(hate == "nothing"):
+    break
+  else:
+      hates.append(hate)
+  #also_hates(name, city, hate)
+userObject.hates = hates
 def saveToParse(anyObject):
     print "Saving..."
 
     anyObject.save()
 
     print "Done!"
+    
+saveToParse(userObject)
